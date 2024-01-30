8ca95ad9efe8a1b6b22795c6c0489ac38cce4959
Gabriele Fontana <gafreax@gmail.com>
2024-01-30 03:16:55 +0100

    refactor: imrpove column len

--- src/lib/table.js
+++ src/lib/table.js
@@ -1,22 +1,29 @@
 import { Table } from 'console-table-printer'
-import { ellipsys } from './text.js'
 
-export const maxColumnLen = (objects) => {
-    const longestString =  objects.reduce((a, c) => {
-        return c?.description?.length - a > 0? c.description.length : a
-    }, 0)
-    return longestString > process.stdout.columns - 10 ? process.stdout.columns - 10 : longestString
+export const colLen = (objects) => {
+    return objects.reduce((a, c) => {
+        const { package: { name, version }} = c
+        return { 
+            nameMaxLen: name.length - a.nameMaxLen> 0 ? name.length : a.nameMaxLen,
+            versionMaxLen: version.length - a.versionMaxLen > 0 ? version.length : a.versionMaxLen
+        }
+    }, {
+        nameMaxLen: 0,
+        versionMaxLen: 0,
+    })
+    
 }
 
 export const buildTable = (objects) => {
-    const maxDescriptionColumns = maxColumnLen(objects)
+    const { nameMaxLen, versionMaxLen } = colLen(objects)
+    const maxDescriptionColumns = process.stdout.columns - nameMaxLen - versionMaxLen - 12
     const p = new Table({
         title: 'Packages',
         sort: (row1, row2) => row2.score - row1.score,
         columns: [
-            { name: 'name', title: 'Name', alignment: 'left', color: 'purple' },
-            { name: 'description', title: 'Description', alignment: 'left', color: 'cyan', printer: (value) => ellipsys(value, maxDescriptionColumns) },
-            { name: 'version', title: 'Version', alignment: 'left' },
+            { name: 'name', title: 'Name', alignment: 'left', color: 'magenta', maxLen: nameMaxLen },
+            { name: 'description', title: 'Description', alignment: 'left', color: 'cyan', maxLen: maxDescriptionColumns},
+            { name: 'version', title: 'Version', alignment: 'left', maxLen: versionMaxLen },
             { name: 'score' }
         ],
         disabledColumns: ['score'],
