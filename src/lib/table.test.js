256e8edda8ba33791ea393e99e5c4a32fa5c275a
Gabriele Fontana <gafreax@gmail.com>
2024-01-30 03:16:32 +0100

    test: table

--- src/lib/table.test.js
+++ src/lib/table.test.js
@@ -1,10 +1,14 @@
-import { maxColumnLen } from "./table.js";
+import { colLen } from "./table.js";
 
 describe('table.js', () => {
     describe('maxColumnLen', () => {
         test('to be the longest decription field', () => {
-            const max = maxColumnLen([{description: 'test'},{description: 'test2'}])
-            expect(max).toBe(5);
+            const max = colLen([{package: {version: '1.0.1', name: 'foo'}},{package: {version: '1.0', name: 'foo bar!'}}])
+            console.dir(max)
+            expect(max).toEqual({
+                nameMaxLen: 8,
+                versionMaxLen: 5
+            });
         })
     })
 })
