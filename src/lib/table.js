5367da3ce8a3c0e8e20ef1c810a4f0c7d1c3e739
Gabriele Fontana <gafreax@gmail.com>
2025-02-23 17:42:15 +0100

    chore: add tests

--- src/lib/table.js
+++ src/lib/table.js
@@ -3,23 +3,23 @@ export const MAXCOL = 50
 
 export const colLen = (objects) => {
   return objects.reduce((a, c) => {
-    const { package: { name, version, links }} = c
-    const nameLen = name.length > MAXCOL ? MAXCOL : name.length
-    const versionLen = version.length > MAXCOL ? MAXCOL : version.length
-    const linkLen = links.npm.length > MAXCOL ? MAXCOL : links.npm.length
+    const { package: { name, version, links } } = c
+    const nameLen = name?.length > MAXCOL ? MAXCOL : name.length
+    const versionLen = version?.length > MAXCOL ? MAXCOL : version.length
+    const linkLen = links?.length > MAXCOL ? MAXCOL : links.length
     return {
       nameMaxLen: nameLen - a.nameMaxLen > 0 ? nameLen : a.nameMaxLen,
       versionMaxLen: versionLen - a.versionMaxLen > 0 ? versionLen : a.versionMaxLen,
-      linkMaxLen: linkLen - a.linkMaxLen > 0 ? linkLen : a.linkMaxLen,
+      linkMaxLen: linkLen - a.linkMaxLen > 0 ? linkLen : a.linkMaxLen
     }
   }, {
     nameMaxLen: 0,
     versionMaxLen: 0,
-    linkMaxLen: 0,
+    linkMaxLen: 0
   })
 }
 
-export const buildLinks = link => link.length > MAXCOL ?  "https://www.npmjs.com/" : link
+export const buildLinks = link => link.length > MAXCOL ? 'https://www.npmjs.com/' : link
 
 export const buildName = name => name.length > MAXCOL ? name.substring(0, name.search(/[^a-z]i/)) : name
 
@@ -28,20 +28,20 @@ export const getColumns = (options, lens) => {
   return [
     { name: 'version', title: 'Version', alignment: 'left', maxLen: versionMaxLen },
     { name: 'name', title: 'Name', alignment: 'left', color: 'magenta', maxLen: nameMaxLen },
-    { name: 'description', title: 'Description', alignment: 'left', color: 'cyan', maxLen: maxDescriptionColumns},
+    { name: 'description', title: 'Description', alignment: 'left', color: 'cyan', maxLen: maxDescriptionColumns },
     { name: 'link', title: 'Link', alignment: 'left', color: 'blue', maxLen: linkMaxLen },
-    { name: 'score', title: 'Score', alignment: 'right', color: 'green' }, 
+    { name: 'score', title: 'Score', alignment: 'right', color: 'green' }
   ]
 }
 
 export const getRows = (objects, options) => {
-  return objects.map( ({ package: { name, description, version, links }, score }) => {
+  return objects.map(({ package: { name, description, version, links }, score }) => {
     const builtName = buildName(name)
     const builtLinks = buildLinks(links.npm)
     return {
       name: builtName,
-      description: description,
-      version: version,
+      description,
+      version,
       link: builtLinks,
       score: score.final
     }
@@ -55,14 +55,14 @@ export const filterColumn = (column, excludeFields, fields) => {
 }
 
 export const buildTable = (objects, options) => {
-  const { excludeFields:x, fields:f, limit } = options
+  const { excludeFields: x, fields: f, limit } = options
   const { nameMaxLen, versionMaxLen, linkMaxLen } = colLen(objects)
   const maxDescriptionColumns = process.stdout.columns - nameMaxLen - versionMaxLen - 20 - linkMaxLen
   const columns = getColumns(options, { nameMaxLen, versionMaxLen, linkMaxLen, maxDescriptionColumns })
   const rows = getRows(objects, options).reduce((a, c, i) => { if (i <= limit) a.push(c); return a }, [])
   const disabledColumns = columns
-    .filter( c => filterColumn(c.name, x, f))
-    .map( c => c.name)
+    .filter(c => filterColumn(c.name, x, f))
+    .map(c => c.name)
   const p = new Table({
     title: 'Packages',
     sort: (row1, row2) => row2.score.final - row1.score.final,
--- src/lib/table.test.js
+++ src/lib/table.test.js
@@ -1,14 +1,15 @@
-import { colLen } from "./table.js";
+import { colLen } from './table.js'
 
 describe('table.js', () => {
-    describe('maxColumnLen', () => {
-        test('to be the longest decription field', () => {
-            const max = colLen([{package: {version: '1.0.1', name: 'foo'}},{package: {version: '1.0', name: 'foo bar!'}}])
-            console.dir(max)
-            expect(max).toEqual({
-                nameMaxLen: 8,
-                versionMaxLen: 5
-            });
-        })
+  describe('maxColumnLen', () => {
+    test('to be the longest decription field', () => {
+      const max = colLen([{ package: { version: '1.0.1', name: 'foo', links: 'npm.or' } }, { package: { version: '1.0', name: 'foo bar!', links: 'npm.org' } }])
+      console.dir(max)
+      expect(max).toEqual({
+        linkMaxLen: 7,
+        nameMaxLen: 8,
+        versionMaxLen: 5
+      })
     })
+  })
 })
